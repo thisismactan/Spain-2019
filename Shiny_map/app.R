68d9661b00b7f7e11d0a6167d64908ad8e9fc85c
Mac Tan <met359@nyu.edu>
2019-03-19 12:48:52 -0400

    Choroplethed the map

--- Shiny_map/app.R
+++ Shiny_map/app.R
@@ -15,14 +15,14 @@ server <- function(input, output) {
   
   output$forecastmap <- renderLeaflet({
     leaflet() %>%
-      addPolygons(data = community_shapes, weight = 1, opacity = 1, color = "#666666", fillOpacity = 1, fillColor = ~color, label = ~community_name, 
+      addPolygons(data = community_shapes, weight = 1, opacity = 1, color = "#666666", fillOpacity = ~opacity, fillColor = ~color, label = ~community_name, 
                   popup = ~community_info, group = "Seats (community)") %>%
-      addPolygons(data = province_shapes, weight = 1, opacity = 1, color = "#666666", fillOpacity = 1, fillColor = ~color, label = ~province_name,
+      addPolygons(data = province_shapes, weight = 1, opacity = 1, color = "#666666", fillOpacity = ~opacity, fillColor = ~color, label = ~province_name,
                   popup = ~province_info, group = "Vote (province)") %>%
       addLayersControl(
         baseGroups = c("Seats (community)", "Vote (province)"),
-        position = "bottomleft",
-        options = layersControlOptions(collapsed = TRUE)
+        position = "topleft",
+        options = layersControlOptions(collapsed = FALSE)
       )
   })
 }
