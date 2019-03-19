c1ce9f9dab25a3513c10ca0b2f15a197166fd4c9
Mac Tan <met359@nyu.edu>
2019-03-19 17:19:58 -0400

    Added coalition analyses

--- Code/coalition_analysis.R
+++ Code/coalition_analysis.R
@@ -19,3 +19,29 @@ simulation_results.natl %>%
             Ciudadanos = mean(ciudadanos_largest),
             Vox = mean(vox_largest)) %>%
   melt(variable.name = "Party", value.name = "Probability")
+
+## Possible coalitions
+
+## Two-party coalitions
+simulation_results.natl %>%
+  group_by(sim_number) %>%
+  spread(party, seats) %>%
+  mutate(`PSOE + UP` = psoe + up > 175,
+         `PSOE + Ciudadanos` = psoe + ciudadanos > 175,
+         `PP + Ciudadanos` = pp + ciudadanos > 175,
+         `PP + Vox` = pp + vox > 175) %>%
+  ungroup() %>%
+  summarise_at(vars(c("PSOE + UP", "PSOE + Ciudadanos", "PP + Ciudadanos", "PP + Vox")), mean) %>%
+  melt(variable.name = "Coalition", value.name = "Prob") %>% 
+  as.tbl()
+
+## Three-party coalitions
+simulation_results.natl %>%
+  group_by(sim_number) %>%
+  spread(party, seats) %>%
+  mutate(`PSOE + UP + Ciudadanos` = psoe + up + ciudadanos > 175,
+         `PP + Ciudadanos + Vox` = pp + ciudadanos + vox > 175) %>%
+  ungroup() %>%
+  summarise_at(vars(c("PSOE + UP + Ciudadanos", "PP + Ciudadanos + Vox")), mean) %>%
+  melt(variable.name = "Coalition", value.name = "Prob") %>%
+  as.tbl()
